7b616edfa010a8c8ea55b2a5ed80c89c7fc53afd
Yamcha973 <jessica_mds_7@hotmail.com>
2020-04-06 15:45:49 +0200

    modif quête5

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,15 +2,32 @@ import React from 'react';
 import './Contact.css';
 import propTypes from 'prop-types';
 
-const Contact = ({name, avatar, online}) => {
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: false,
+    };
+  }
+  render() {
     return (
-  <div className="Contact">
-    <img className="avatar" src={avatar} />
-    <div className="status name"> <p>{name}</p>
-    <p className="status text">{online ? <span className="status-online"></span> : <span className="status-offline"></span>} {online ? "Online" : "Offline"}</p>
+  <div className="Contact"
+  onClick={event => {
+    const newOnline = !this.state.online;
+    this.setState({ online: newOnline });
+}}>
+    <img className="avatar" src={this.props.avatar} alt="avatar" />
+    <div className="status name"> <p>{this.props.name}</p>
+    <p className="status text"> <span className={this.state.online ? 'status-online' : 'status-offline'}
+      
+>
+</span>
+{this.state.online ? 'Online' : 'Offline'}
+    </p>
     </div>
     </div>
   );
+}
 }
 
 Contact.propTypes = {
